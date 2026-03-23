afe70cead5911fb22d0eb5830035dd39145f203d
Shivam <51397757+shivamg-engineer@users.noreply.github.com>
2026-03-23 15:40:11 +0530

    Delete React/1.1 directory

--- React/1.1/my-app/src/component/ParentComponent.jsx
+++
@@ -1,9 +0,0 @@
-const ParentComponent=({name})=>{
-    return <ChildComponent name={name}/>
-}
-
-const ChildComponent = ({ name }) => {
-  return <h1>Welcome, {name}!</h1>;
-};
-
-export default ParentComponent;
